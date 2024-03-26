1a84fd4522362a2f4a7e59c540421b1427f035e2
austine-2020 <ogston02@gmail.com>
2024-03-26 14:40:26 -0700

    Binary trees

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+94% of storage used … If you run out, you can't create, edit, and upload files. Get 100 GB of storage for ₦950.00 ₦470.00/month for 6 months.
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
+}
